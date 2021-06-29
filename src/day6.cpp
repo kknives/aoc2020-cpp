7a9162552663c4bda18f43fe90354e23e39d8a7e
Shashwat Ganesh <shashwatganesh@protonmail.com>
2021-06-29 20:57:48 +0530

    Implement common search; solve part 2

--- src/day6.cpp
+++ src/day6.cpp
@@ -14,6 +14,19 @@ auto unique_answers(Answers &ans, std::string grp_ans) -> Answers::size_type {
   return ans.size();
 }
 
+auto common_answers(AnsMap &ans, std::string grp_ans) -> AnsMap::size_type {
+  using namespace ranges;
+
+  int member_count;
+  for (auto&& [i, str_v] : grp_ans | views::split('\n') | views::enumerate) {
+    for ( char x : str_v) {
+      ans[x]++;
+    }
+    member_count = i+1; // Number not index
+  };
+  return count_if(ans, [member_count](auto const& pair) { const auto& [key, count] = pair; return count == member_count;});
+}
+
 std::istream& operator>> (std::istream& is, std::string& op) {
   std::string line;
   while( std::getline(is, line) && !line.empty()) {
@@ -25,15 +38,20 @@ std::istream& operator>> (std::istream& is, std::string& op) {
 
 int main() {
   Answers unique{};
-  auto sum = 0;
+  AnsMap common{};
+  auto uniques = 0;
+  auto commons = 0;
 
   std::string grp_ans;
   while(std::cin >> grp_ans) {
     // fmt::print("dbg: parsed {}\n", grp_ans);
-    sum += unique_answers(unique, grp_ans);
+    uniques += unique_answers(unique, grp_ans);
+    commons += common_answers(common, grp_ans);
     unique.clear();
+    common.clear();
     grp_ans.clear();
   }
-  fmt::print("The answer is: {}\n", sum);
+  fmt::print("#Unique Answers = {}\n", uniques);
+  fmt::print("#Common Answers = {}\n", commons);
 
 }
